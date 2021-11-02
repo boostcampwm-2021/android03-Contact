70475fafcc8ef3b5140f26a6fa844af38d261d2c
Lee WonJoong <realjoong@gmail.com>
2021-11-02 14:03:18 +0900

    [FEAT] #4 - Bottom Navigation View, 프래그먼트 연결

--- app/src/main/java/com/ivyclub/contact/MainActivity.kt
+++ app/src/main/java/com/ivyclub/contact/MainActivity.kt
@@ -1,7 +1,23 @@
 package com.ivyclub.contact
 
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ivyclub.contact.databinding.ActivityMainBinding
 import com.ivyclub.contact.util.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        val navView: BottomNavigationView = binding.bnvMain
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+    }
 }
---
+++ app/src/main/java/com/ivyclub/contact/ui/main/friend/FriendFragment.kt
@@ -0,0 +1,8 @@
+package com.ivyclub.contact.ui.main.friend
+
+import com.ivyclub.contact.R
+import com.ivyclub.contact.databinding.FragmentFriendBinding
+import com.ivyclub.contact.util.BaseFragment
+
+class FriendFragment: BaseFragment<FragmentFriendBinding>(R.layout.fragment_friend) {
+}
