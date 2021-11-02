9fee2e3c477d5441a77587afb4c75bb31e91cae5
ddanglehee <shee9812@naver.com>
2021-11-02 18:07:47 +0900

    [FEAT] #2 - DateUtil 추가

---
+++ app/src/main/java/com/ivyclub/contact/util/DateUtil.kt
@@ -0,0 +1,21 @@
+package com.ivyclub.contact.util
+
+import java.sql.Date
+import java.util.*
+
+fun Date.getExactYear(): Int = getCalendar(this).get(Calendar.YEAR)
+
+fun Date.getExactMonth(): Int = getCalendar(this).get(Calendar.MONTH) + 1
+
+fun Date.getDayOfMonth(): Int = getCalendar(this).get(Calendar.DAY_OF_MONTH)
+
+fun Date.getDayOfWeek(): DayOfWeek {
+    return DayOfWeek.values()[getCalendar(this).get(Calendar.DAY_OF_WEEK) - 1]
+}
+
+private fun getCalendar(date: Date): Calendar {
+    val calendar = Calendar.getInstance()
+    calendar.time = date
+
+    return calendar
+}
