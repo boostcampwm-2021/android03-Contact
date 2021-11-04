dba32a15e4424e5f38c8073991a3991a7db394a1
tyehooney <xogns4313@naver.com>
2021-11-04 16:00:45 +0900

    [FEAT] #9 - PlanViewModel 생성 및 PlanFragment에 적용

--- app/src/main/java/com/ivyclub/contact/ui/main/plan/PlanFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/plan/PlanFragment.kt
@@ -1,8 +1,19 @@
 package com.ivyclub.contact.ui.main.plan
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.ivyclub.contact.R
 import com.ivyclub.contact.databinding.FragmentPlanBinding
 import com.ivyclub.contact.util.BaseFragment
 
 class PlanFragment : BaseFragment<FragmentPlanBinding>(R.layout.fragment_plan) {
+
+    private val viewModel: PlanViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
+    }
 }
---
+++ app/src/main/java/com/ivyclub/contact/ui/main/plan/PlanViewModel.kt
@@ -0,0 +1,7 @@
+package com.ivyclub.contact.ui.main.plan
+
+import androidx.lifecycle.ViewModel
+
+class PlanViewModel : ViewModel() {
+
+}
