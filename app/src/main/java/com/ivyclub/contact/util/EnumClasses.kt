935d892a99de96c19c6e4b4cee6638ab1a9d1861
tyehooney <xogns4313@naver.com>
2021-11-04 16:43:18 +0900

    [FEAT] #9 - DayOfWeek enum class korean 값 수정('요일' 제거)

--- app/src/main/java/com/ivyclub/contact/util/EnumClasses.kt
+++ app/src/main/java/com/ivyclub/contact/util/EnumClasses.kt
@@ -1,13 +1,13 @@
 package com.ivyclub.contact.util
 
 enum class DayOfWeek(val value: Int) : KoreanTranslatable {
-    SUN(1) { override val korean = "일요일" },
-    MON(2) { override val korean = "월요일" },
-    TUE(3) { override val korean = "화요일" },
-    WED(4) { override val korean = "수요일" },
-    THU(5) { override val korean = "목요일" },
-    FRI(6) { override val korean = "금요일" },
-    SAT(0) { override val korean = "토요일" }
+    SUN(1) { override val korean = "일" },
+    MON(2) { override val korean = "월" },
+    TUE(3) { override val korean = "화" },
+    WED(4) { override val korean = "수" },
+    THU(5) { override val korean = "목" },
+    FRI(6) { override val korean = "금" },
+    SAT(0) { override val korean = "토" }
 }
 
 interface KoreanTranslatable {
