e40ca3ead03fe5133d9452189a81c13b25a86007
tyehooney <xogns4313@naver.com>
2021-11-08 15:35:02 +0900

    [CHORE] #35 - build.gradle에 navigation safe args 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
