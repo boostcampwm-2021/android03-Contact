16ea2ea4e312fb06d7ea0d31d9b1aa4ca72fe7d7
tyehooney <xogns4313@naver.com>
2021-11-08 18:42:29 +0900

    [FEAT] #35 - PlanDetailsViewModel에 약속 상세 정보 불러오는 기능 추가

--- app/src/main/java/com/ivyclub/contact/ui/main/plan_details/PlanDetailsFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/plan_details/PlanDetailsFragment.kt
@@ -3,17 +3,37 @@ package com.ivyclub.contact.ui.main.plan_details
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.ivyclub.contact.R
 import com.ivyclub.contact.databinding.FragmentPlanDetailsBinding
 import com.ivyclub.contact.util.BaseFragment
+import com.ivyclub.contact.util.setFriendChips
+import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
 
+@AndroidEntryPoint
 class PlanDetailsFragment : BaseFragment<FragmentPlanDetailsBinding>(R.layout.fragment_plan_details) {
 
     private val viewModel: PlanDetailsViewModel by viewModels()
+    private val args: PlanDetailsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.viewModel = viewModel
+        binding.dateFormat = SimpleDateFormat(getString(R.string.format_simple_date))
+
+        fetchPlanDetails()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        viewModel.planParticipants.observe(viewLifecycleOwner) {
+            binding.cgPlanParticipants.setFriendChips(it)
+        }
+    }
+
+    private fun fetchPlanDetails() {
+        viewModel.getPlanDetails(args.planId)
     }
 }
--- app/src/main/java/com/ivyclub/contact/ui/main/plan_details/PlanDetailsViewModel.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/plan_details/PlanDetailsViewModel.kt
@@ -3,8 +3,50 @@ package com.ivyclub.contact.ui.main.plan_details
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ivyclub.data.ContactRepository
+import com.ivyclub.data.model.PlanData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.sql.Date
+import javax.inject.Inject
 
-class PlanDetailsViewModel : ViewModel() {
+@HiltViewModel
+class PlanDetailsViewModel @Inject constructor(
+    private val repository: ContactRepository
+) : ViewModel() {
 
+    private val _planDetails = MutableLiveData<PlanData>()
+    val planDetails: LiveData<PlanData> = _planDetails
 
+    private val _planParticipants = MutableLiveData<List<String>>()
+    val planParticipants: LiveData<List<String>> = _planParticipants
+
+    fun getPlanDetails(planId: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val planData = repository.getPlanDetailsById(planId)
+            if (planData != null) {
+                val friends = mutableListOf<String>()
+                planData.participant.forEach {
+                    friends.add(repository.getFriendNameByPhoneNumber(it))
+                }
+                _planParticipants.postValue(friends)
+                _planDetails.postValue(planData)
+            } else { // temp
+                val tmpPlanData = PlanData(
+                    0L,
+                    emptyList(),
+                    Date(System.currentTimeMillis()),
+                    "부스트캠프 모임",
+                    "서울시 강남구",
+                    "프로젝트 회의, 저녁식사",
+                    ""
+                )
+                val friends = listOf("홍길동", "철수", "영희", "맹구", "태훈")
+                _planParticipants.postValue(friends)
+                _planDetails.postValue(tmpPlanData)
+            }
+        }
+    }
 }
