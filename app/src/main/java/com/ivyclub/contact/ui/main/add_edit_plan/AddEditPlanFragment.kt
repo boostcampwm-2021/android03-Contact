f8a24d0bdca534349e80aa462b04a3c9377b5d50
tyehooney <xogns4313@naver.com>
2021-11-09 21:07:01 +0900

    [FEAT] #37 - AddEditPlanFragment에 지인 추가 입력란 자동완성 처리 기능 추가

--- app/src/main/java/com/ivyclub/contact/ui/main/add_edit_plan/AddEditPlanFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/add_edit_plan/AddEditPlanFragment.kt
@@ -6,13 +6,14 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.ivyclub.contact.R
 import com.ivyclub.contact.databinding.FragmentAddEditPlanBinding
-import com.ivyclub.contact.databinding.FragmentPlanDetailsBinding
 import com.ivyclub.contact.util.BaseFragment
+import com.ivyclub.contact.util.addChips
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 
 @AndroidEntryPoint
-class AddEditPlanFragment : BaseFragment<FragmentAddEditPlanBinding>(R.layout.fragment_add_edit_plan) {
+class AddEditPlanFragment :
+    BaseFragment<FragmentAddEditPlanBinding>(R.layout.fragment_add_edit_plan) {
 
     private val viewModel: AddEditPlanViewModel by viewModels()
     private val args: AddEditPlanFragmentArgs by navArgs()
@@ -22,5 +23,53 @@ class AddEditPlanFragment : BaseFragment<FragmentAddEditPlanBinding>(R.layout.fr
 
         binding.viewModel = viewModel
         binding.dateFormat = SimpleDateFormat(getString(R.string.format_simple_date))
+
+        checkFrom()
+        setButtonClickListeners()
+        setObservers()
+    }
+
+    private fun setButtonClickListeners() {
+        with(binding) {
+            ivBtnEditPlanFinish.setOnClickListener {
+                // TODO: 일정 저장
+            }
+            ivBtnDeletePlan.setOnClickListener {
+                // TODO: 일정 삭제
+            }
+        }
+    }
+
+    private fun checkFrom() {
+        if (args.planId != -1L) viewModel.getLastPlan(args.planId)
+    }
+
+    private fun setObservers() {
+        viewModel.friendList.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty() && binding.actPlanParticipants.adapter == null) {
+                setAutoCompleteAdapter(it)
+            }
+        }
+
+        viewModel.planParticipants.observe(viewLifecycleOwner) {
+            binding.flPlanParticipants.addChips(it.map { pair -> pair.second }) { index ->
+                viewModel.removeParticipant(index)
+            }
+        }
+    }
+
+    private fun setAutoCompleteAdapter(friendList: List<Pair<String, String>>) {
+        if (context == null) return
+        val autoCompleteAdapter = FriendAutoCompleteAdapter(requireContext(), friendList)
+        with(binding.actPlanParticipants) {
+            setOnItemClickListener { _, _, i, l ->
+                (adapter as FriendAutoCompleteAdapter).getItem(i)?.let {
+                    viewModel.addParticipant(it)
+                    text = null
+                }
+            }
+
+            setAdapter(autoCompleteAdapter)
+        }
     }
 }
