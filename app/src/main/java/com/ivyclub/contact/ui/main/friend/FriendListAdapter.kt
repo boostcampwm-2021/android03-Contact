58b10b3ee1de9c76b36f5b27199dc60bbd9adf12
Lee WonJoong <realjoong@gmail.com>
2021-11-09 16:57:15 +0900

    [CHORE] #15 - 필요 없는 import문 제거

--- app/src/main/java/com/ivyclub/contact/ui/main/friend/FriendListAdapter.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/friend/FriendListAdapter.kt
@@ -1,11 +1,7 @@
 package com.ivyclub.contact.ui.main.friend
 
 import android.util.Log
-import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
