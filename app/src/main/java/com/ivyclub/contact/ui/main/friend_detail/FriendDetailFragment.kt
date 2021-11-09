9063f10a2dd1118135465d20d1bf4425bc3c80b2
K054_정우진 <79732057+1600094806@users.noreply.github.com>
2021-11-09 12:09:14 +0900

    [LAYOUT] #41 - 지인 초기화면

---
+++ app/src/main/java/com/ivyclub/contact/ui/main/friend_detail/FriendDetailFragment.kt
@@ -0,0 +1,15 @@
+package com.ivyclub.contact.ui.main.friend_detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ivyclub.contact.R
+import com.ivyclub.contact.databinding.FragmentFriendDetailBinding
+import com.ivyclub.contact.util.BaseFragment
+
+class FriendDetailFragment : BaseFragment<FragmentFriendDetailBinding>(R.layout.fragment_friend_detail){
+
+
+}
