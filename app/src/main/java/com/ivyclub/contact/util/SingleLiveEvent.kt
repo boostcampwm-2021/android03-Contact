9de096186a60686bfee6974acfef588c9f088a70
tyehooney <xogns4313@naver.com>
2021-11-10 15:53:23 +0900

    [FEAT]#38 - SingleLiveEvent 추가

---
+++ app/src/main/java/com/ivyclub/contact/util/SingleLiveEvent.kt
@@ -0,0 +1,31 @@
+package com.ivyclub.contact.util
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val isPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun setValue(value: T?) {
+        isPending.set(true)
+        super.setValue(value)
+    }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, {
+            if (isPending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        })
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
