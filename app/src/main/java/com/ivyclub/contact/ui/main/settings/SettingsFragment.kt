c3708c0d453cb053eec1eb05a4066935419d40c4
Lee WonJoong <realjoong@gmail.com>
2021-11-16 23:56:44 +0900

    [FEAT] #106 - 뒤로가기 버튼 구현

--- app/src/main/java/com/ivyclub/contact/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/settings/SettingsFragment.kt
@@ -1,8 +1,22 @@
 package com.ivyclub.contact.ui.main.settings
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.ivyclub.contact.R
 import com.ivyclub.contact.databinding.FragmentSettingsBinding
 import com.ivyclub.contact.util.BaseFragment
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBackIcon()
+    }
+
+    private fun initBackIcon() {
+        binding.ivBackIcon.setOnClickListener {
+            val navController = findNavController()
+            navController.popBackStack()
+        }
+    }
 }
