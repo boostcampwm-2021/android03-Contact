b0f973e0f0fe3a8876e0811c4d22780db0674897
Lee WonJoong <realjoong@gmail.com>
2021-11-17 16:28:19 +0900

    [CHORE] #106 - 코드 정리

--- app/src/main/java/com/ivyclub/contact/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/settings/SettingsFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.ivyclub.contact.R
-import com.ivyclub.contact.databinding.FragmentDialogNotificationTimeBinding
 import com.ivyclub.contact.databinding.FragmentSettingsBinding
 import com.ivyclub.contact.ui.main.settings.dialog.NotificationTimeDialogFragment
-import com.ivyclub.contact.ui.onboard.notification.NotificationTimeFragment
-import com.ivyclub.contact.ui.onboard.notification.NotificationTimeFragmentDirections
 import com.ivyclub.contact.util.BaseFragment
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
