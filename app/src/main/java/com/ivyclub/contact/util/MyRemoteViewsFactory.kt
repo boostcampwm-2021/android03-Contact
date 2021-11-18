e11cd5b9716c41820295ebbbd7111aacb8c1d571
tyehooney <xogns4313@naver.com>
2021-11-18 11:36:22 +0900

    [FIX] #129 - 위젯 아이템 조회 flow 적용

--- app/src/main/java/com/ivyclub/contact/util/MyRemoteViewsFactory.kt
+++ app/src/main/java/com/ivyclub/contact/util/MyRemoteViewsFactory.kt
@@ -4,50 +4,63 @@ import android.content.Context
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import com.ivyclub.data.model.SimplePlanData
-import java.util.Date
 import com.ivyclub.contact.R
 import com.ivyclub.data.ContactRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.sql.Date
 
 class MyRemoteViewsFactory(
     private val context: Context,
     private val repository: ContactRepository
-) :
-    RemoteViewsService.RemoteViewsFactory {
+) : RemoteViewsService.RemoteViewsFactory {
 
-    private var data = listOf<SimplePlanData>()
+    private val data = mutableListOf<SimplePlanData>()
 
     init {
         CoroutineScope(Dispatchers.IO).launch {
-            data = repository.getPlanList().filter { it.date > Date() }.sortedBy { it.date }
+            getPlanListForWidget()
         }
     }
 
-    override fun onCreate() {
+    private suspend fun getPlanListForWidget() {
+        val current = Date(System.currentTimeMillis())
 
+        repository.loadPlanListWithFlow().buffer().collect { newPlanList ->
+            data.clear()
+            data.addAll(newPlanList.filter {
+                it.date.getExactYear() == current.getExactYear() &&
+                    it.date.getExactMonth() == current.getExactMonth()
+            })
+        }
     }
+
+    override fun onCreate() {}
 
     override fun onDataSetChanged() {
         CoroutineScope(Dispatchers.IO).launch {
-            data = repository.getPlanList().filter { it.date > Date() }.sortedBy { it.date }
+            getPlanListForWidget()
         }
     }
 
-    override fun onDestroy() {
+    override fun onDestroy() {}
 
-    }
-
-    override fun getCount(): Int {
-        return data.size
-    }
+    override fun getCount() = data.size
 
     override fun getViewAt(position: Int): RemoteViews {
         val listviewWidget = RemoteViews(context.packageName, R.layout.item_widget)
+        val planDate = data[position].date
+        val dateText = String.format(
+            context.getString(R.string.format_date_day),
+            planDate.getDayOfMonth(),
+            planDate.getDayOfWeek().korean
+        )
         listviewWidget.setTextViewText(
             R.id.tv_widget_plan_date,
-            data[position].date.toString()
+            dateText
         )
         listviewWidget.setTextViewText(R.id.tv_widget_plan_title, data[position].title)
         return listviewWidget
