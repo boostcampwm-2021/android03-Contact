9a43c0a7ea4d323e7059876d7432d334f5147ca8
Lee WonJoong <realjoong@gmail.com>
2021-11-22 00:45:25 +0900

    [FEAT] #153 - try catch문 runCatching으로 개선

--- data/src/main/java/com/ivyclub/data/ImageManager.kt
+++ data/src/main/java/com/ivyclub/data/ImageManager.kt
@@ -2,33 +2,32 @@ package com.ivyclub.data
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
-import java.lang.Exception
+import kotlin.runCatching
 
 object ImageManager {
     fun saveProfileImage(currentBitmap: Bitmap?, friendId: Long) {
-        if(currentBitmap == null) return
-        val tempFile = File(IMAGE_FILE_PATH,"$friendId.jpg")
-        try {
+        if (currentBitmap == null) return
+        val tempFile = File(IMAGE_FILE_PATH, "$friendId.jpg")
+        runCatching {
             tempFile.createNewFile()
             val out = FileOutputStream(tempFile)
             currentBitmap.compress(Bitmap.CompressFormat.JPEG, 50, out)
             out.close()
-        } catch (e: Exception) {
-
+        }.onFailure { exception ->
+            Log.e("ImageManagerError", "Exception : $exception")
         }
     }
 
     fun loadProfileImage(friendId: Long): Bitmap? {
-        return try {
+        return runCatching {
             BitmapFactory.decodeFile("$IMAGE_FILE_PATH$friendId.jpg")
-        } catch(e: OutOfMemoryError) {
-            null
-        }
+        }.mapCatching { loadedImage ->
+            loadedImage
+        }.getOrDefault(null)
     }
 
-
     private const val IMAGE_FILE_PATH = "/data/user/0/com.ivyclub.contact/cache/"
-
 }
