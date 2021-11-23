fb5239257f4261f412c1a0705614363753c304b7
ddanglehee <shee9812@naver.com>
2021-11-24 00:52:28 +0900

    [CHORE] 필요 없는 코드 삭제

--- app/src/main/java/com/ivyclub/contact/ui/main/settings/group/GroupListAdapter.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/settings/group/GroupListAdapter.kt
@@ -1,7 +1,6 @@
 package com.ivyclub.contact.ui.main.settings.group
 
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
