2d72e67eb4a06150cf0f3af29baf19b28c7fa8a0
tyehooney <xogns4313@naver.com>
2021-11-23 14:57:56 +0900

    [FEAT] DB 버전 1 -> 2 마이그레이션 적용

--- data/src/main/java/com/ivyclub/data/repository/ContactDatabase.kt
+++ data/src/main/java/com/ivyclub/data/repository/ContactDatabase.kt
@@ -10,6 +10,41 @@ import com.ivyclub.data.model.GroupData
 
 @Database(entities = [FriendData::class, GroupData::class, PlanData::class], version = 2)
 @TypeConverters(RoomConverters::class)
-abstract class ContactDatabase: RoomDatabase() {
+abstract class ContactDatabase : RoomDatabase() {
     abstract fun contactDAO(): ContactDAO
+
+    companion object {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(
+                    """CREATE TABLE `tmp_friendData` (
+                        `phoneNumber` TEXT NOT NULL, 
+                        `name` TEXT NOT NULL, 
+                        `birthday` TEXT NOT NULL, 
+                        `groupId` INTEGER NOT NULL, 
+                        `planList` TEXT NOT NULL, 
+                        `isFavorite` INTEGER NOT NULL, 
+                        `extraInfo` TEXT NOT NULL, 
+                        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+                        )"""
+                        .trimMargin()
+                )
+
+                database.execSQL("DROP TABLE FriendData")
+                database.execSQL("ALTER TABLE tmp_friendData RENAME TO FriendData")
+
+                database.execSQL(
+                    """CREATE TABLE `tmp_groupData` (
+                        `name` TEXT NOT NULL,
+                        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+                        )"""
+                        .trimMargin()
+                )
+
+                database.execSQL("DROP TABLE GroupData")
+                database.execSQL("ALTER TABLE tmp_groupData RENAME TO GroupData")
+                database.execSQL("DELETE FROM PlanData")
+            }
+        }
+    }
 }
