0216bc5e2ef5befa2aa8e588e5a51286a72fbde9
Lee WonJoong <realjoong@gmail.com>
2021-11-23 22:37:08 +0900

    [CHORE] #168 - 온보딩 문장 수정

--- app/src/main/java/com/ivyclub/contact/ui/onboard/app_intro/IntroFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/onboard/app_intro/IntroFragment.kt
@@ -1,7 +1,6 @@
 package com.ivyclub.contact.ui.onboard.app_intro
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
@@ -13,7 +12,7 @@ class IntroFragment : BaseFragment<FragmentIntroBinding>(R.layout.fragment_intro
     private val introImages =
         listOf(R.drawable.intro0, R.drawable.intro1, R.drawable.intro2, R.drawable.intro3)
     private val introString = listOf(
-        "안녕하세요\n친구와 지인의 정보를 기록할 수 있는\n컨택트입니다.",
+        "안녕하세요\n친구와 지인의 정보를\n기록할 수 있는 컨택트입니다.",
         "지인과의 약속을 저장해주시면,\n기억하고 있다가 알림으로 알려드릴게요.",
         "걱정하지 마세요.\n컨택을 보호하기 위해 비밀번호와\n지문인식 기능을 이용할 수 있어요.",
         "지금 바로 시작해봐요.",
@@ -21,7 +20,6 @@ class IntroFragment : BaseFragment<FragmentIntroBinding>(R.layout.fragment_intro
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.e("temp", ".${getString(R.string.fragment_intro_introduce_string_4)}")
         initPage()
     }
 
@@ -40,5 +38,4 @@ class IntroFragment : BaseFragment<FragmentIntroBinding>(R.layout.fragment_intro
     private fun navigateToNext() {
         findNavController().navigate(IntroFragmentDirections.actionIntroFragmentToNotificationTimeFragment())
     }
-
 }
