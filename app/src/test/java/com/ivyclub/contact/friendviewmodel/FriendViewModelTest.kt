65536763ae237ccb5471c2da0c0562a139c3525c
Lee WonJoong <realjoong@gmail.com>
2021-11-27 16:18:39 +0900

    [FEAT] #226 - setSearchViewVisibility 테스트코드 작성

--- app/src/test/java/com/ivyclub/contact/friendviewmodel/FriendViewModelTest.kt
+++ app/src/test/java/com/ivyclub/contact/friendviewmodel/FriendViewModelTest.kt
@@ -29,10 +29,22 @@ class FriendViewModelTest {
 
     @Test
     fun invokeClearLongClickedId_returnLongClickedIdEmpty() {
-        // when clearLongClickedId()가 호출되었을 때
+        // when : clearLongClickedId()가 호출되었을 때
         viewModel.clearLongClickedId()
 
-        // then longClickedId 리스트가 클리어 된다
+        // then : longClickedId 리스트가 클리어 된다
         assertTrue(viewModel.longClickedId.isEmpty())
+    }
+
+    @Test
+    fun invokeSetSearchViewVisibility_returnChangedVisibility() {
+        // given : 현재 searchViewVisible을 originSearchViewVisibility 저장
+        val originSearchViewVisibility = viewModel.isSearchViewVisible.value
+
+        // when : setSearchViewVisibility()가 호출되었을 때
+        viewModel.setSearchViewVisibility()
+
+        // then : isSearchViewVisibility가 변경되었는지 확인
+        assertTrue(viewModel.isSearchViewVisible.value != originSearchViewVisibility)
     }
 }
