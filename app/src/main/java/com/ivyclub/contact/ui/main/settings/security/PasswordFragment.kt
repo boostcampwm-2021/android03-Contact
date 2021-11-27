c798e2b171ff85aac9714a6faac02098c2a38e9d
ddanglehee <shee9812@naver.com>
2021-11-27 11:34:34 +0900

    [FIX] #225 - 비밀번호 화면에서 키보드 안뜨도록 수정

--- app/src/main/java/com/ivyclub/contact/ui/main/settings/security/PasswordFragment.kt
+++ app/src/main/java/com/ivyclub/contact/ui/main/settings/security/PasswordFragment.kt
@@ -43,6 +43,7 @@ class PasswordFragment :
         initBackPressedListener()
         observeFocusedEditTextIndex()
         observeShowSnackBar()
+        blockKeyboard()
     }
 
     private fun checkFingerPrintState() {
@@ -165,6 +166,14 @@ class PasswordFragment :
     private fun observeShowSnackBar() {
         viewModel.showSnackBar.observe(viewLifecycleOwner) { id ->
             Snackbar.make(binding.root, getString(id), Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun blockKeyboard() {
+        passwordEditTextList.forEach {
+            it.setTextIsSelectable(true)
+            it.showSoftInputOnFocus = false
+            it.isFocusableInTouchMode = false
         }
     }
 
