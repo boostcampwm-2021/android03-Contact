73d877b5dfb33e797e4edb6f794f90ae6f1a97fd
Lee WonJoong <realjoong@gmail.com>
2021-12-03 17:19:52 +0900

    [FEAT] #258 - plan list date format 분기문으로 다국어 처리

--- app/src/main/java/com/ivyclub/contact/util/StringManager.kt
+++ app/src/main/java/com/ivyclub/contact/util/StringManager.kt
@@ -5,7 +5,7 @@ import java.util.*
 object StringManager {
     fun getString(targetString: String): String {
         when (Locale.getDefault().language) {
-            "kr" -> { // 한국어일 때
+            "ko" -> { // 한국어일 때
                 return targetString
             }
             else -> { // 영어이거나 지원되지 않는 언어일 때
@@ -22,6 +22,14 @@ object StringManager {
                     else -> "-"
                 }
             }
+        }
+    }
+
+    fun getDateFormatBy(planDayOfMonth: String, planDayOfWeek: String): String {
+        return if (Locale.getDefault().language == "ko") {
+            "${planDayOfMonth}일 ${planDayOfWeek}요일"
+        } else {
+            "$planDayOfMonth $planDayOfWeek"
         }
     }
 }
