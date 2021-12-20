54705c6add98963778514921bf10adfc976c05dc
tyehooney <xogns4313@naver.com>
2021-12-20 10:05:51 +0900

    [REFACTOR] #267 - WorkManagerModule, PlanReminderNotificationWorker 제거

--- app/src/main/java/com/ivyclub/contact/MainApplication.kt
+++ app/src/main/java/com/ivyclub/contact/MainApplication.kt
@@ -1,18 +1,7 @@
 package com.ivyclub.contact
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application(), Configuration.Provider{
-
-    @Inject lateinit var workerFactory: HiltWorkerFactory
-
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-}
+class MainApplication : Application()
